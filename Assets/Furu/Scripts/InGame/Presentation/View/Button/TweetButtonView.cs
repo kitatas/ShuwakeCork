f8056b4da9812df503c8023cc03fa2304bd939ca
kitatas <315kitatas10703@gmail.com>
2023-07-04 23:08:50 +0900

    fix: TweetButtonViewのリファクタ

--- Assets/Furu/Scripts/InGame/Presentation/View/Button/TweetButtonView.cs
+++ Assets/Furu/Scripts/InGame/Presentation/View/Button/TweetButtonView.cs
@@ -1,4 +1,5 @@
 using Furu.Base.Presentation.View;
+using Furu.Common;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -9,26 +10,23 @@ namespace Furu.InGame.Presentation.View
         public void SetUpDistanceTweet(float value, string liquidName)
         {
             var tweetText = $"{liquidName}で距離 {value.ToString("F2")}m 吹っ飛ばした！\n";
-#if UNITY_WEBGL
-            tweetText += $"#{GameConfig.GAME_ID} #unity1week\n";
-            pushed += () => UnityRoomTweet.Tweet(GameConfig.GAME_ID, tweetText);
-#elif UNITY_ANDROID
-            tweetText += $"#{GameConfig.GAME_ID}\n";
-            // TODO: tweetText += $"{UrlConfig.APP}";
-            var url = $"https://twitter.com/intent/tweet?text={UnityWebRequest.EscapeURL(tweetText)}";
-            pushed += () => Application.OpenURL(url);
-#endif
+            Tweet(tweetText);
         }
 
         public void SetUpHeightTweet(float value, string liquidName)
         {
             var tweetText = $"{liquidName}で高さ {value.ToString("F2")}m まで吹っ飛ばした！\n";
+            Tweet(tweetText);
+        }
+
+        private void Tweet(string tweetText)
+        {
 #if UNITY_WEBGL
-            tweetText += $"#{GameConfig.GAME_ID} #unity1week\n";
-            pushed += () => UnityRoomTweet.Tweet(GameConfig.GAME_ID, tweetText);
+            // tweetText += $"#{GameConfig.GAME_ID} #unity1week\n";
+            // pushed += () => UnityRoomTweet.Tweet(GameConfig.GAME_ID, tweetText);
 #elif UNITY_ANDROID
             tweetText += $"#{GameConfig.GAME_ID}\n";
-            // TODO: tweetText += $"{UrlConfig.APP}";
+            tweetText += $"{UrlConfig.APP}";
             var url = $"https://twitter.com/intent/tweet?text={UnityWebRequest.EscapeURL(tweetText)}";
             pushed += () => Application.OpenURL(url);
 #endif
